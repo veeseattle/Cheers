6db524712c2fbbafc5467167b4fc5faee2da8e11
Vania Kurniawati <vania.baek@gmail.com>
2015-03-20 14:05:17 -0700

    incorporated further changes to apple pay integration to include applepaystubs (simulator helper) and sending token to server

--- Cheers/OrderingViewController.m
+++ Cheers/OrderingViewController.m
@@ -10,9 +10,14 @@
 #import "BartenderViewController.h"
 #import "NetworkController.h"
 #import "Order.h"
+#import "Stripe.h"
+#import "STPTestPaymentAuthorizationViewController.h"
+
+
 @import PassKit;
 
-@interface OrderingViewController ()
+@interface OrderingViewController () <PKPaymentAuthorizationViewControllerDelegate, STPCheckoutViewControllerDelegate>
+
 @property (weak, nonatomic) IBOutlet UIPickerView *drinksPicker;
 @property (strong, nonatomic) NSArray *drinksArray;
 @property (weak, nonatomic) IBOutlet UILabel *recipe;
@@ -32,7 +37,7 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   
-  self.paymentNetwork = (PKPaymentNetworkAmex, PKPaymentNetworkMasterCard, PKPaymentNetworkVisa);
+  self.paymentNetwork = [NSArray arrayWithObjects:PKPaymentNetworkAmex, PKPaymentNetworkMasterCard, PKPaymentNetworkVisa, nil];
   self.applePayMerchantID = @"merchant.cheers";
   
   self.navigationItem.title = @"Bars";
@@ -118,9 +123,7 @@
   [[NetworkController sharedService] postDrinkOrder:drinkID];
   
   //create simple PKPaymentRequest object that represents a single Apple Pay payment
-  PKPaymentRequest *request = [[PKPaymentRequest alloc] init];
-  PKPaymentAuthorizationViewController *applePayController = [[PKPaymentAuthorizationViewController alloc] initWithPaymentRequest:request];
-  [self presentViewController:applePayController animated:true completion:nil];
+  PKPaymentRequest *request = [Stripe paymentRequestWithMerchantIdentifier:self.applePayMerchantID];
   
   //setup for transaction capabilities
   request.merchantIdentifier = self.applePayMerchantID;
@@ -130,20 +133,88 @@
   request.currencyCode = @"USD";
   
   //setup for payment summary
-  NSDecimalNumber *subtotalAmount = [NSDecimalNumber decimalNumberWithMantissa:6 exponent:-2 isNegative:NO]; //replace 6 with actual drink price
+  NSDecimalNumber *subtotalAmount = [NSDecimalNumber decimalNumberWithMantissa:600 exponent:-2 isNegative:NO]; //replace 6 with actual drink price
   self.subtotal = [PKPaymentSummaryItem summaryItemWithLabel:@"Subtotal" amount:subtotalAmount];
   
   NSDecimalNumber *totalAmount = [NSDecimalNumber zero];
   totalAmount = [totalAmount decimalNumberByAdding:subtotalAmount];
   self.total = [PKPaymentSummaryItem summaryItemWithLabel:@"Unicorn - Capitol Hill" amount:totalAmount]; //replace Unicorn - Capitol Hill with actual bar name
   
-  request.paymentSummaryItems = @[self.subtotal, self.total];
+  request.paymentSummaryItems = [NSArray arrayWithObjects:self.subtotal, self.total, nil];
   
+  if ([Stripe canSubmitPaymentRequest:request]) {
+    
+    STPTestPaymentAuthorizationViewController *paymentController = [[STPTestPaymentAuthorizationViewController alloc]
+                                                                    initWithPaymentRequest:request];
+    paymentController.delegate = self;
+    
+    [self presentViewController:paymentController animated:YES completion:nil];
+    
+    
+  }
+  else {
+    
+  };
   
+  PKPaymentAuthorizationViewController *applePayController = [[PKPaymentAuthorizationViewController alloc] initWithPaymentRequest:request];
+  
+  [self presentViewController:applePayController animated:true completion:nil];
   
   
   NSLog(@"Posted to the database");
   
+}
+
+// MARK: - PKPaymentAuthorizationViewControllerDelegate
+
+- (void) paymentAuthorizationViewController:(PKPaymentAuthorizationViewController *)controller didAuthorizePayment:(PKPayment *)payment completion:(void (^)(PKPaymentAuthorizationStatus))completion {
+  //use Stripe SDK to finish charging customer
+  [self handlePaymentAuthorizationWithPayment:payment completion:completion];
+  
+  //when this is done, call completion(PKPaymentAuthorizationStatus.Success)
+};
+
+- (void) paymentAuthorizationViewControllerDidFinish:(PKPaymentAuthorizationViewController *)controller {
+  [controller dismissViewControllerAnimated:true completion:nil];
+};
+
+- (void)handlePaymentAuthorizationWithPayment:(PKPayment *)payment
+                                   completion:(void (^)(PKPaymentAuthorizationStatus))completion {
+  [[STPAPIClient sharedClient] createTokenWithPayment:payment
+                                           completion:^(STPToken *token, NSError *error) {
+                                             if (error) {
+                                               completion(PKPaymentAuthorizationStatusFailure);
+                                               return;
+                                             }
+                                             /*
+                                              We'll implement this below in "Sending the token to your server".
+                                              Notice that we're passing the completion block through.
+                                              See the above comment in didAuthorizePayment to learn why.
+                                              */
+                                             [self createBackendChargeWithToken:token completion:completion];
+                                           }];
+}
+
+
+- (void)createBackendChargeWithToken:(STPToken *)token
+                          completion:(void (^)(PKPaymentAuthorizationStatus))completion {
+  NSURL *url = [NSURL URLWithString:@"https://example.com/token"];
+  NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
+  request.HTTPMethod = @"POST";
+  NSString *body     = [NSString stringWithFormat:@"stripeToken=%@", token.tokenId];
+  request.HTTPBody   = [body dataUsingEncoding:NSUTF8StringEncoding];
+  
+  [NSURLConnection sendAsynchronousRequest:request
+                                     queue:[NSOperationQueue mainQueue]
+                         completionHandler:^(NSURLResponse *response,
+                                             NSData *data,
+                                             NSError *error) {
+                           if (error) {
+                             completion(PKPaymentAuthorizationStatusFailure);
+                           } else {
+                             completion(PKPaymentAuthorizationStatusSuccess);
+                           }
+                         }];
 }
 
 @end
--- Cheers/ViewController.m
+++ Cheers/ViewController.m
@@ -19,8 +19,9 @@
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
   
-
+  
 }
+
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
